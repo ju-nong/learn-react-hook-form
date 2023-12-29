bb561a2c72bd6f694158a6af810a9d2dd05da825
이준용 <iiiuooyoo@gmail.com>
2023-12-29 14:04:38 +0900

    update : 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,128 +1,13 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-
-type Inputs = {
-    id: string;
-    password: string;
-    checkbox: string[];
-    radio: string;
-    select: string;
-};
-
-const inputsDefaultValues: Inputs = {
-    id: "",
-    password: "",
-    checkbox: ["중식"],
-    radio: "M",
-    select: "혈액형",
-};
+import { Basic } from "./components";
 
 function App() {
-    const {
-        register,
-        handleSubmit,
-        // watch,
-        // formState: { errors },
-    } = useForm<Inputs>({
-        defaultValues: inputsDefaultValues,
-    });
-
-    const [state, setState] = useState<Inputs>(inputsDefaultValues);
-
-    function onSubmit(data: Inputs) {
-        setState((state) => ({ ...state, ...data }));
-    }
-
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label>
-                <span>input[type="text"]</span>
-                <input type="text" placeholder="id" {...register("id")} />
-            </label>
+        <>
+            <h2>기본 submit 실습</h2>
+            <Basic />
 
-            <label>
-                <span>input[type="password"]</span>
-                <input
-                    type="password"
-                    placeholder="password"
-                    {...register("password")}
-                />
-            </label>
-            <label>
-                <span>password confirm</span>
-                <input
-                    type="password"
-                    placeholder="password confirm"
-                    {...register("password")}
-                />
-            </label>
-
-            <div>
-                <span>input[type="checkbox"]</span>
-                <label>
-                    <input
-                        type="checkbox"
-                        defaultValue="한식"
-                        {...register("checkbox")}
-                    />
-                    한식
-                </label>
-
-                <label>
-                    <input
-                        type="checkbox"
-                        defaultValue="중식"
-                        {...register("checkbox")}
-                    />
-                    중식
-                </label>
-                <label>
-                    <input
-                        type="checkbox"
-                        defaultValue="양식"
-                        {...register("checkbox")}
-                    />
-                    양식
-                </label>
-            </div>
-
-            <div>
-                <span>input[type="radio"]</span>
-                <label>
-                    <input
-                        type="radio"
-                        defaultValue="M"
-                        {...register("radio")}
-                    />
-                    남성
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        defaultValue="W"
-                        {...register("radio")}
-                    />
-                    여성
-                </label>
-            </div>
-
-            <label>
-                <span>select</span>
-                <select {...register("select")}>
-                    <option selected disabled>
-                        혈액형
-                    </option>
-                    <option>A</option>
-                    <option>B</option>
-                    <option>O</option>
-                    <option>AB</option>
-                </select>
-            </label>
-
-            <button>제출</button>
-
-            {JSON.stringify(state)}
-        </form>
+            <h2>닉네임 중복 확인</h2>
+        </>
     );
 }
 
---
+++ src/components/Basic.tsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+    id: string;
+    password: string;
+    checkbox: string[];
+    radio: string;
+    select: string;
+};
+
+const inputsDefaultValues: Inputs = {
+    id: "",
+    password: "",
+    checkbox: ["중식"],
+    radio: "M",
+    select: "혈액형",
+};
+
+function Basic() {
+    const { register, handleSubmit } = useForm<Inputs>({
+        defaultValues: inputsDefaultValues,
+    });
+
+    const [state, setState] = useState<Inputs>(inputsDefaultValues);
+
+    function onSubmit(data: Inputs) {
+        setState((state) => ({ ...state, ...data }));
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+                <span>input[type="text"]</span>
+                <input type="text" placeholder="id" {...register("id")} />
+            </label>
+
+            <label>
+                <span>input[type="password"]</span>
+                <input
+                    type="password"
+                    placeholder="password"
+                    {...register("password")}
+                />
+            </label>
+
+            <div>
+                <span>input[type="checkbox"]</span>
+                <label>
+                    <input
+                        type="checkbox"
+                        defaultValue="한식"
+                        {...register("checkbox")}
+                    />
+                    한식
+                </label>
+
+                <label>
+                    <input
+                        type="checkbox"
+                        defaultValue="중식"
+                        {...register("checkbox")}
+                    />
+                    중식
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        defaultValue="양식"
+                        {...register("checkbox")}
+                    />
+                    양식
+                </label>
+            </div>
+
+            <div>
+                <span>input[type="radio"]</span>
+                <label>
+                    <input
+                        type="radio"
+                        defaultValue="M"
+                        {...register("radio")}
+                    />
+                    남성
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        defaultValue="W"
+                        {...register("radio")}
+                    />
+                    여성
+                </label>
+            </div>
+
+            <label>
+                <span>select</span>
+                <select {...register("select")}>
+                    <option selected disabled>
+                        혈액형
+                    </option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>O</option>
+                    <option>AB</option>
+                </select>
+            </label>
+
+            <button>제출</button>
+
+            {JSON.stringify(state)}
+        </form>
+    );
+}
+
+export { Basic as default };
